3739a4c76c5d9326e1b9403c2cc5b6b3a9d4be73
Mitrajeet-Golsangi <mitrajeetgolsangi@gmail.com>
2022-10-08 21:38:28 +0530

    Frontend framework boilerplate

---
+++ frontend/utils/authentication.js
@@ -0,0 +1,7 @@
+/**
+ * Logs in the user in order to continue
+ *
+ * @param {string} email Email of the User
+ * @param {string} password Password of the User
+ */
+export const login = (email, password) => {};
